ee4f36eb83ea0056e68cc2444698c7cc7fca34e4
Yasin Ertekin <yasinertekinn@outlook.com>
2024-01-15 05:22:47 +0300

    Add CustomLightTheme and CustomTheme classes

---
+++ lib/product/initialize/theme/custom_light_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:subscriptionfairy/product/initialize/theme/color_schemes.g.dart';
+import 'package:subscriptionfairy/product/initialize/theme/custom_theme.dart';
+
+/// CustomLightTheme
+final class CustomLightTheme implements CustomTheme {
+  @override
+  ThemeData get themeData => ThemeData(
+        useMaterial3: true,
+        colorScheme: CustomColorScheme.lightColorScheme,
+      );
+}
---
+++ lib/product/initialize/theme/custom_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+/// An abstract class representing a custom theme configuration.
+abstract class CustomTheme {
+  /// The primary [ThemeData] for the application.
+  ThemeData get themeData;
+}
