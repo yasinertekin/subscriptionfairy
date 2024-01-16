bcd172a9e53b6b24658bea172cca1a0782054f3b
Yasin <yasinertekin24@gmail.com>
2024-01-16 13:47:33 +0300

    Add home view and view model for displaying user subscriptions

--- lib/feature/home/home_view.dart
+++ lib/feature/home/home_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:subscriptionfairy/feature/home/view_model/home_cubit.dart';
+import 'package:subscriptionfairy/feature/home/view_model/home_state.dart';
+import 'package:subscriptionfairy/product/widget/custom_cached_network_image.dart';
 
 /// This is the view for the home feature.
 final class HomeView extends StatelessWidget {
@@ -7,9 +11,73 @@ final class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Home',
+        ),
+      ),
+      body: BlocBuilder<UsersCubit, UsersState>(
+        builder: (context, state) {
+          if (state is UsersInitialState) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is UsersLoadedState) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: state.users.subscriptionList?.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          leading: CustomCachedNetworkImage(
+                            imageUrl: state.users.subscriptionList?[index]
+                                    .subscriptionIcon ??
+                                '',
+                            height: 100,
+                            width: 100,
+                          ),
+                          title: Text(
+                            state.users.subscriptionList?[index]
+                                    .subscriptionPlan ??
+                                '',
+                          ),
+                          subtitle: Text(
+                            state.users.subscriptionList?[index]
+                                    .subscriptionPlanId
+                                    ?.toString() ??
+                                '',
+                          ),
+                          trailing: Switch(
+                            onChanged: (value) {},
+                            value: state.users.subscriptionList?[index]
+                                    .subscriptionPlanId ??
+                                false,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          } else if (state is UsersErrorState) {
+            return Center(
+              child: Text(
+                state.error,
+              ),
+            );
+          } else {
+            return const Center(
+              child: Text(
+                'Something went wrong!',
+              ),
+            );
+          }
+        },
       ),
     );
   }
---
+++ lib/feature/home/view_model/home_cubit.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+// Create the UsersCubit class
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:subscriptionfairy/feature/home/view_model/home_state.dart';
+import 'package:subscriptionfairy/product/firebase/firebase_auth_service_manager.dart';
+import 'package:subscriptionfairy/product/service/firebase_firestore_service_interface.dart';
+
+class UsersCubit extends Cubit<UsersState> {
+  /// Default constructor
+  UsersCubit(this.firebaseService) : super(UsersLoadingState()) {
+    getUsersSubscriptions();
+  }
+
+  /// FirebaseFireStoreServiceInterface
+  final FirebaseFireStoreServiceInterface firebaseService;
+
+  /// Get users subscriptions
+
+  Future<void> getUsersSubscriptions() async {
+    try {
+      emit(UsersInitialState()); // Set initial state
+
+      final users = await firebaseService.getUsersSubscriptions(
+        FirebaseAuthServiceManager
+            .instance.firebaseAuthService.firebaseAuth.currentUser!.uid,
+      );
+
+      // Check if the retrieved Users object is not empty
+      emit(UsersLoadedState(users));
+    } catch (e) {
+      emit(UsersErrorState('Error fetching user subscriptions: $e'));
+    }
+  }
+}
