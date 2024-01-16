7fbca9970328a715ce58f82a439f11f056df500b
Yasin <yasinertekin24@gmail.com>
2024-01-16 04:23:29 +0300

    Add DashboardView for bottom navigation bar feature

--- lib/feature/dashboard/dashboard_view.dart
+++ lib/feature/dashboard/dashboard_view.dart
@@ -1,17 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+// ignore: implementation_imports
 import 'package:gen/src/index.dart';
+import 'package:kartal/kartal.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:subscriptionfairy/feature/home/home_view.dart';
 import 'package:subscriptionfairy/feature/subscriptions/subscriptions_view.dart';
 
 /// This is the view for the bottom navigation bar feature.
-final class CustomBottomNavigationBar extends StatelessWidget {
-  /// This is the constructor for the bottom navigation bar view.
-  const CustomBottomNavigationBar({super.key});
+final class DashboardView extends StatelessWidget {
+  /// Constructor of DashboardView
+  const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const milliSeconds = 200;
     final controller = PersistentTabController();
     return PersistentTabView(
       navBarStyle: NavBarStyle.style15,
@@ -22,12 +25,14 @@ final class CustomBottomNavigationBar extends StatelessWidget {
       resizeToAvoidBottomInset: true,
       popAllScreensOnTapAnyTabs: true,
       decoration: NavBarDecoration(
-        borderRadius: BorderRadius.circular(20),
-        colorBehindNavBar: Colors.white,
+        borderRadius: context.border.lowBorderRadius,
+        colorBehindNavBar: ColorName.colorWhite,
       ),
       itemAnimationProperties: const ItemAnimationProperties(
         // Navigation Bar's items animation properties.
-        duration: Duration(milliseconds: 200),
+        duration: Duration(
+          milliseconds: milliSeconds,
+        ),
         curve: Curves.ease,
       ),
       screenTransitionAnimation: const ScreenTransitionAnimation(
@@ -42,7 +47,9 @@ List<Widget> _buildScreens() {
   return [
     const HomeView(),
     const SubscriptionsView(),
-    const Text('dfsdfsdf'),
+    Center(
+      child: Text(FirebaseAuth.instance.currentUser?.uid ?? 'No user'),
+    ),
   ];
 }
 
