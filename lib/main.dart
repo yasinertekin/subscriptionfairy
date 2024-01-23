68bc6bab609d8d43f4eff2589f03682518a1d013
Yasin Ertekin <yasinertekinn@outlook.com>
2024-01-23 03:07:05 +0300

    Add imports and initialize packages and services

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:subscriptionfairy/firebase_options.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kartal/kartal.dart';
+import 'package:subscriptionfairy/product/core/theme/theme.dart';
+import 'package:subscriptionfairy/product/core/theme/theme_state.dart';
+import 'package:subscriptionfairy/product/initialize/app_environment.dart';
+import 'package:subscriptionfairy/product/initialize/localization/project_localization.dart';
 import 'package:subscriptionfairy/product/initialize/navigation/navigation_service.dart';
 import 'package:subscriptionfairy/product/initialize/navigation/routes.dart';
 import 'package:subscriptionfairy/product/initialize/state/state_initialize.dart';
-import 'package:subscriptionfairy/product/initialize/theme/custom_light_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  print('widget flutter binding ensure initialized');
+  await EasyLocalization.ensureInitialized();
+  print('easy localization ensure initialized');
+  await DeviceUtility.instance.initPackageInfo();
+  print('device utility instance init package info');
+
+  AppEnvironment.general();
+  print('app environment general');
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: AppEnvironment.firebaseOptions,
   );
+  print('firebase initialize app');
   runApp(
-    const StateInitialize(
-      child: _MyApp(),
+    StateInitialize(
+      child: ProductLocalization(
+        child: const _MyApp(),
+      ),
     ),
   );
 }
@@ -24,12 +40,19 @@ final class _MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: CustomLightTheme().themeData,
-      routes: Routes.getRouteTable(),
-      initialRoute: Routes.initialRoute,
-      navigatorKey: NavigationService.instance.navigatorKey,
+    return BlocBuilder<ThemeCubit, ThemeState>(
+      builder: (context, state) {
+        return MaterialApp(
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          title: 'Flutter Demo',
+          theme: state.themeData,
+          routes: Routes.getRouteTable(),
+          initialRoute: Routes.initialRoute,
+          navigatorKey: NavigationService.instance.navigatorKey,
+        );
+      },
     );
   }
 }
