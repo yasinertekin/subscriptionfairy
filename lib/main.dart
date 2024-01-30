fcc1462d31caf386dde536745d46b91c3e76e23a
Yasin <yasinertekin24@gmail.com>
2024-01-31 02:30:50 +0300

    Refactor imports and initialize app in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kartal/kartal.dart';
-import 'package:subscriptionfairy/firebase_options.dart';
 import 'package:subscriptionfairy/product/core/theme/theme.dart';
 import 'package:subscriptionfairy/product/core/theme/theme_state.dart';
-import 'package:subscriptionfairy/product/initialize/app_environment.dart';
+import 'package:subscriptionfairy/product/initialize/app_initialize.dart';
 import 'package:subscriptionfairy/product/initialize/localization/project_localization.dart';
 import 'package:subscriptionfairy/product/initialize/navigation/navigation_service.dart';
 import 'package:subscriptionfairy/product/initialize/navigation/routes.dart';
 import 'package:subscriptionfairy/product/initialize/state/state_initialize.dart';
+import 'package:subscriptionfairy/product/utility/constants/string_constants/string_constants.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
-  await DeviceUtility.instance.initPackageInfo();
-
-  AppEnvironment.general();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await AppInitialize.initialize();
   runApp(
     StateInitialize(
       child: ProductLocalization(
@@ -43,7 +34,7 @@ final class _MyApp extends StatelessWidget {
           localizationsDelegates: context.localizationDelegates,
           supportedLocales: context.supportedLocales,
           locale: context.locale,
-          title: 'Flutter Demo',
+          title: StringConstants.appName,
           theme: state.themeData,
           routes: Routes.getRouteTable(),
           initialRoute: Routes.initialRoute,
